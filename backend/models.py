c894d021f480748ef027cdf82fcaad7d56fcf85d
Marius Nicolescu <marius.nick@gmail.com>
2022-09-16 15:25:46 +0300

    add secure env

--- backend/models.py
+++ backend/models.py
@@ -2,9 +2,13 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
+from dotenv import load_dotenv
 
-database_name = 'trivia'
-database_path = 'postgresql://{}/{}'.format('localhost:5432', database_name)
+load_dotenv()
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER= os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+database_path = 'postgresql://{}:{}@{}/{}'.format(DB_USER,DB_PASSWORD,'localhost:5432', DB_NAME)
 
 db = SQLAlchemy()
 
